6f1d72ea7350a145a843c45ea7c1fa5c2026aeba
pausbsixeight <57077195+pausbsixeight@users.noreply.github.com>
2021-05-02 14:40:15 +0200

    Update main.py
    fastapi

--- main.py
+++ main.py
@@ -1,44 +1,18 @@
-import random
-import requests
-from bs4 import BeautifulSoup
+from typing import Optional
 
-# crawl IMDB Top 250 and randomly select a movie
+from fastapi import FastAPI
+import uvicorn
 
-URL = 'http://www.imdb.com/chart/top'
+app = FastAPI()
 
-def main():
-    response = requests.get(URL)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    #soup = BeautifulSoup(response.text, 'lxml') # faster
 
-    # print(soup.prettify())
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
 
-    movietags = soup.select('td.titleColumn')
-    inner_movietags = soup.select('td.titleColumn a')
-    ratingtags = soup.select('td.posterColumn span[name=ir]')
-
-    def get_year(movie_tag):
-        moviesplit = movie_tag.text.split()
-        year = moviesplit[-1] # last item 
-        return year
-
-    years = [get_year(tag) for tag in movietags]
-    actors_list =[tag['title'] for tag in inner_movietags] # access attribute 'title'
-    titles = [tag.text for tag in inner_movietags]
-    ratings = [float(tag['data-value']) for tag in ratingtags] # access attribute 'data-value'
-
-    n_movies = len(titles)
-
-    while(True):
-        idx = random.randrange(0, n_movies)
-        
-        print(f'{titles[idx]} {years[idx]}, Rating: {ratings[idx]:.1f}, Starring: {actors_list[idx]}')
-
-        user_input = input('Do you want another movie (y/[n])? ')
-        if user_input != 'y':
-            break
-    
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
